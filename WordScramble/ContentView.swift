45243508393725e2da98158cc05e50b03bd748e6
Nicole Kong <mningk@gmail.com>
2026-09-26 00:50:22 +0800

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Nicole Kong Mei Ning on 26/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Nicole Kong Mei Ning on 26/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
